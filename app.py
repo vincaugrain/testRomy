8bd06c5d9dca62fb13b1eacb2e026ecafef1517d
Augrain <v.augrain@romy.paris>
2018-01-09 15:32:50 +0100

    fixed acccess token

--- app.py
+++ app.py
@@ -36,12 +36,12 @@ def parseRes(data):
 	    i = i + 1
 	return ret
 
-
 def processRequest(req):
     if req.get("result").get("action") != "get_prescription":
         return {}
     baseurl = "https://staging-app.api.romy-paris.com/google/api/prescription"
-    url = baseurl + "&format=json"
+    accessToken = req.get('originalRequest').get('data').get('user').get('accessToken')
+    url = baseurl + "&format=json" + "&accessToken=" + str(accessToken)
     result = urlopen(url).read()
     last = result.get_json(silent=True, force=True)
     print(print(json.dumps(last, indent=4)))
